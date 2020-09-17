7e093bebb4bdd47752508853323aee89e78487d3
cheetosysst <thect@protonmail.com>
2020-09-17 11:08:27 +0800

    Comment and remove print()

--- fandom.py
+++ fandom.py
@@ -1,38 +1,46 @@
+# fandom.py
+# This script is not the final version. It's only for testing purpose.
+
+# Library
+# Install dependencies with "pip3 install beautifulsoup4 requests"
 import requests
 import sys
 import os
 from bs4 import BeautifulSoup
 
+# Variables
 url = ""
 
+# Basic argument parsing
+# Bugs included, lol.
 try:
 	url = sys.argv[1]
 except:
 	print("[] Error: No URL found. Exiting")
 	exit()
-
 if "fandom" not in url.split("."):
 	print("[] Error: No wiki page. Exiting")
 	exit()
-
 if "wiki" not in url.split("/"):
 	print("[] Error: Page format not supported. Exiting")
 	exit()
 
-
+# wiki data
 wikiRaw = requests.get(url+"?action=edit")
 wikiTitle = url.split("/")[-1]
 wikiParsed = BeautifulSoup(wikiRaw.text, 'html.parser')
-
 path = os.path.join(os.getcwd(), wikiTitle + ".txt")
 
+# show wiki data to user
 print("[] Wiki info")
 print("\ttitle:", wikiTitle)
 print("\turl  :", url)
 print("\tpath :", path)
 
+# Write file
 file = open(path, "w+", encoding="utf-8")
 wikitext = wikiParsed.find("textarea", {"id": "wpTextbox1"})
-print(wikitext)
 file.write(wikitext.text)
 file.close()
+
+#  Bye
